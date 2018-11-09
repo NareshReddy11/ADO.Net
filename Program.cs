2adc8a8d0d826029c870f1a38c4ff6d828cbc8f9
NareshReddy11 <nareshreddy.v1994@gmail.com>
2018-11-09 21:28:15 +0530

    ADO-net

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace AdoNetConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=.; database=NareshReddy; integrated security=SSPI");
+                SqlCommand cm = new SqlCommand("create table student(id int not null, name varchar(100), email varchar(50), join_date date)", con);
+                con.Open();
+                cm.ExecuteNonQuery(); 
+                Console.WriteLine("Table created Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
